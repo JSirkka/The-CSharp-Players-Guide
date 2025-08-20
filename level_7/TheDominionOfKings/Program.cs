cf8300311666a580de93a721ccdcfb23fcd5562c
unknown <jonas.ostenstad@student.uib.no>
2025-08-20 12:21:31 +0200

    The dominion of kings

---
+++ level_7/TheDominionOfKings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Amount of provinces:");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Amount of dutchies:");
+int dutchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Amount of estates:");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+int totalScore = (provinces * 6) + (dutchies * 3) + estates;
+
+Console.WriteLine($"Total score is {totalScore}");
