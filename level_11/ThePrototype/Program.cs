d47c71d4a1d93c8dfa5cf576ae8c42ac4daf750c
unknown <jonas.ostenstad@student.uib.no>
2025-08-22 16:58:25 +0200

    The Prototype

---
+++ level_11/ThePrototype/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int targetNum;
+
+do
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    targetNum = Convert.ToInt32(Console.ReadLine());
+
+}
+while (targetNum < 0 || targetNum > 100);
+
+Console.Clear();
+
+Console.WriteLine("User 2, guess the number");
+
+int guess;
+
+while (true)
+{
+    Console.Write("What is your next guess? ");
+    guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess == targetNum)
+    {
+        Console.WriteLine("You guessed the number!");
+        break;
+    }
+    else if (guess < targetNum)
+    {
+        Console.WriteLine($"{guess} is too low.");
+    }
+    else if (guess > targetNum)
+    {
+        Console.WriteLine($"{guess} is too high.");
+    }
+}
