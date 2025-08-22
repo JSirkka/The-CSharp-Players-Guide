542d43c13f8bb36ad8d1a0b0e1ad468416ab5f9a
unknown <jonas.ostenstad@student.uib.no>
2025-08-22 15:17:15 +0200

    Watchtower

---
+++ level_9/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Input X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x < 0)
+{
+    if (y > 0) Console.WriteLine("NW");
+    else if (y == 0) Console.WriteLine("W");
+    else if (y < 0) Console.WriteLine("SW");
+}
+else if (x == 0)
+{
+    if (y > 0) Console.WriteLine("N");
+    else if (y == 0) Console.WriteLine("!");
+    else if (y < 0) Console.WriteLine("S");
+}
+else if (x > 0)
+{
+    if (y > 0) Console.WriteLine("NE");
+    else if (y == 0) Console.WriteLine("E");
+    else if (y < 0) Console.WriteLine("SE");
+}
