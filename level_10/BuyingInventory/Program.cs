b36624d5423429d27d5521513ed5404e4d040319
unknown <jonas.ostenstad@student.uib.no>
2025-08-22 15:45:44 +0200

    Buying inventory

---
+++ level_10/BuyingInventory/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(
+    "The following items are available: \n" +
+    "1 - Rope\n" +
+    "2 - Torches\n" +
+    "3 - Climbing Equipment\n" +
+    "4 - Clean Water\n" +
+    "5 - Machete\n" +
+    "6 - Canoe\n" +
+    "7 - Food Supplies\n"
+    );
+
+Console.Write("What number do you want to see the price of? ");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+string response = choice switch
+{
+    1 => "Rope cost 10 gold",
+    2 => "Torches cost 15 gold",
+    3 => "Climbing Equipment cost 25 gold",
+    4 => "Clean Water cost 1 gold",
+    5 => "Machete cost 20 gold",
+    6 => "Canoe cost 200 gold",
+    7 => "Food Suplies cost 1 gold",
+    _ => $"Unknown choice: {choice}"
+};
+
+Console.WriteLine(response);
