959b3c7299acd5d297fcbb440afb8a245ddd68f9
unknown <jonas.ostenstad@student.uib.no>
2025-08-22 10:52:25 +0200

    Defense of Consolas

---
+++ level_8/DefenseOfConsolas/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Title = "Defense Of Consolas";
+
+Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({row}, {col - 1})");
+Console.WriteLine($"({row - 1}, {col})");
+Console.WriteLine($"({row}, {col + 1})");
+Console.WriteLine($"({row + 1}, {col})");
+
+Console.Beep();
