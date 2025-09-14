ebb42b7a9295b0f86c714060cc73bedae43a526d
unknown <jonas.ostenstad@student.uib.no>
2025-09-14 14:52:04 +0200

    The Laws of Freach

---
+++ level_12/TheLawsOfFreach/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.  
+foreach (int num in array)
+{
+    if (num < currentSmallest)
+        currentSmallest = num;
+}
+Console.WriteLine(currentSmallest);
+
+int total = 0;
+foreach (int num in array)
+    total += num;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
