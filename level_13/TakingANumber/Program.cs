04db163028e915e05351ade904e6ced219ecbb26
unknown <jonas.ostenstad@student.uib.no>
2025-09-15 17:15:39 +0200

    level 13

---
+++ level_13/TakingANumber/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int askForNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int askForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int num = askForNumber(text);
+        
+        if(num > min && num < max)
+        {
+            return num;
+        }
+    }
+}
+
+
+Console.WriteLine(askForNumberInRange("Give me number between 1 and 10", 1, 10));
---
+++ level_13/recursiveCount/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+void recursiveCount(int n)
+{
+    if (n == 0) return;
+    
+    Console.WriteLine(n);
+    recursiveCount(n - 1);
+}
+
+
+recursiveCount(10);
